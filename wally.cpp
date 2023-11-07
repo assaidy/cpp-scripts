69d1b134ae7a931a1fbf57b958a856c844ba2de4
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-11-07 11:01:30 +0200

    adding automatic change wallpaper 	modified:   wally.cpp

--- wally.cpp
+++ wally.cpp
@@ -1,8 +1,10 @@
+#include <chrono>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <filesystem>
 #include <string>
+#include <thread>
 #include <vector>
 
 namespace fs = std::filesystem;
@@ -10,21 +12,27 @@ using std::string;
 using std::vector;
 
 // get a list of all files
-vector<string> get_dir_files(string path) {
+vector<string> get_dir_files(const string &_path) {
     vector<string> files;
-    for (const auto &entry : fs::directory_iterator(path))
+    for (const auto &entry : fs::directory_iterator(_path))
         files.push_back(entry.path());
     return files;
 }
 
 // get a random file
 string get_rand_file(vector<string> files) {
-    srand(time(0));
     return files.at(rand() % files.size());
+}
+
+void set_wal(const string &_choosen_bg) {
+    const auto feh_cmd { "feh --bg-fill " + _choosen_bg };
+    system(feh_cmd.c_str());
 }
 
 int main()
 {
+    srand(time(0));
+
     // declare the path
     string HOME { getenv("HOME") };
     string path { HOME + "/Pictures/walls" };
@@ -32,12 +40,19 @@ int main()
     // get all files
     auto files { get_dir_files(path) };
 
-    // print random file
+    // choose random file
     string choosen_bg { get_rand_file(files) };
 
-    // set as background
-    const auto feh_cmd { "feh --bg-fill " + choosen_bg };
-    system(feh_cmd.c_str());
+    // set as background (once)
+    // set_wal(choosen_bg);
+
+    // automatically change wallpaper (work in bg)
+    while (true) {
+        set_wal(choosen_bg);
+        const std::chrono::minutes DURATION(1);
+        std::this_thread::sleep_for(DURATION);
+        choosen_bg = get_rand_file(files);
+    }
 
     // set pywal
     // const auto pywal_cmd { "wal -i " + choosen_bg };
