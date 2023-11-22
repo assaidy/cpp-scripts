b3f6bc72869a3a393b8a37f4549a7b60efd96016
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-11-22 10:10:35 +0200

    adding cat clone 	new file:   mycat.cpp

---
+++ mycat.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void help_msg() {
+    std::cout << "[Error] invalid argument.\n";
+    std::cout << "Usage: mycat [PATH]\n";
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2) {
+        std::string path {argv[1]};
+        std::fstream file_handler(path.c_str());
+        if (file_handler.fail()) {
+            std::cout << "[Error] cannot open the file '" << path << "'.\n";
+            return 1;
+        } else {
+            std::string line;
+            while (std::getline(file_handler, line))
+                std::cout << line << "\n";
+        }
+        file_handler.close();
+    }
+    else {
+        help_msg();
+    }
+    return 0;
+}
